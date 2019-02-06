4c17d925f0d035fb7f6e3ccba86cc6ed6c96d0a8
samansoltani <34808539+samansoltani@users.noreply.github.com>
2019-02-06 18:16:35 -0500

    card and dice classes update.

--- SOEN6441/src/Model/Card.java
+++ SOEN6441/src/Model/Card.java
@@ -1,4 +1,9 @@
 package Model;
+/**
+ * This class handles all actions about card.
+ * @author samansoltani
+ * @version 1.0
+ */
 
 public class Card {
 
--- SOEN6441/src/Model/Dice.java
+++ SOEN6441/src/Model/Dice.java
@@ -1,4 +1,9 @@
 package Model;
+/**
+ * This class handles the dice actions.
+ * @author samansoltani
+ *@version 1.0
+ */
 
 public class Dice {
 
