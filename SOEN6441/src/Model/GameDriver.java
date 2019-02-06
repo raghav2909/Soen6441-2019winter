e51ceeb2646f6880c3d9e3c4a2aaeb2ef2ee1e16
samansoltani <34808539+samansoltani@users.noreply.github.com>
2019-02-06 18:18:57 -0500

    Map and gamedriver classes update.

--- SOEN6441/src/Model/GameDriver.java
+++ SOEN6441/src/Model/GameDriver.java
@@ -1,5 +1,9 @@
 package Model;
-
+/**
+ * This class handles the game driver.
+ * @author samansoltani
+ *@version 1.0
+ */
 public class GameDriver {
 
 }
--- SOEN6441/src/Model/Map.java
+++ SOEN6441/src/Model/Map.java
@@ -1,5 +1,9 @@
 package Model;
-
+/**
+ * This class handles the map.
+ * @author samansoltani
+ *@version 1.0
+ */
 public class Map {
 
 }
