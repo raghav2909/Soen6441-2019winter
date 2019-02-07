bd422f55fe547fd92b27972be79a29f7356bccc2
Yaswanth <yaswanthchoudary.n@gmil.com>
2019-02-06 20:30:17 -0500

    Gamephase class methods inital

--- SOEN6441/src/Model/GamePhase.java
+++ SOEN6441/src/Model/GamePhase.java
@@ -5,5 +5,27 @@ package Model;
  * version 1.0
  */
 public class GamePhase {
+	
+	/**
+	 * method for attack phase
+	 */
+	public void aphase() {
+		
+	}
+	
+	/**
+	 * method for reinforcement phase
+	 */
+	public void rphase() {
+		
+	}
+	
+	/**
+	 * method for fortification phase.
+	 */
+	public void fphase() {
+		
+	}
+	
 
 }
