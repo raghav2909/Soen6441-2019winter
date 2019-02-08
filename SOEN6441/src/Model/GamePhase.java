26b9ed822cb627e873fa0475e53493e66861daf0
Yaswanth <yaswanthchoudary.n@gmil.com>
2019-02-08 17:29:52 -0500

    GamePhase class update

--- SOEN6441/src/Model/GamePhase.java
+++ SOEN6441/src/Model/GamePhase.java
@@ -6,24 +6,37 @@ package Model;
  */
 public class GamePhase {
 	
+	String phasename;
+	
+	GamePhase attack=new GamePhase("attack");
+	GamePhase reinforcement=new GamePhase("reinforcement");
+	GamePhase fortification=new GamePhase("fortification");
+	
+	
+	public GamePhase(String string) {
+		// TODO Auto-generated constructor stub
+		this.phasename= string;
+	}
+
 	/**
 	 * method for attack phase
 	 */
 	public void aphase() {
-		
+		this.phasename="attack";
 	}
 	
 	/**
 	 * method for reinforcement phase
 	 */
 	public void rphase() {
-		
+		this.phasename="reinforcement";
 	}
 	
 	/**
 	 * method for fortification phase.
 	 */
 	public void fphase() {
+		this.phasename="fortification";
 		
 	}
 	
