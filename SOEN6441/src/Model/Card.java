0f8bc3fc2f4c573cb5457537719169421318af83
samansoltani <34808539+samansoltani@users.noreply.github.com>
2019-02-25 19:12:56 -0500

    card class update

--- SOEN6441/src/Model/Card.java
+++ SOEN6441/src/Model/Card.java
@@ -5,6 +5,51 @@ package Model;
  * @version 1.0
  */
 
-public class Card {
-
+public class Card
+{
+	/**
+	 * Save the name of the card
+	 */
+	String Name;
+	
+	
+	/**
+	 * Save the type of the card
+	 */
+	String Type;
+	
+	
+	/**
+	 * Set up a card with name and type
+	 * @param Name the name of card
+	 * @param Type the type of the card
+	 */
+	Card (String Name ,String Type)
+	{
+		this.Name = Name;
+		this.Type = Type;
+	}
+	
+	
+	
+	/**
+	 * getting the name of card
+	 * @return the name of card
+	 */
+	String getName() 
+	{
+		return this.Name;
+	}
+	
+	
+	/**
+	 * getting the type of card
+	 * @return the type of card
+	 */
+	String getType() 
+	{
+		return this.Type;
+	}
+	
+	
 }
