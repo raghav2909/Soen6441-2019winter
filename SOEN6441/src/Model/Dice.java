c8e96c97679658f68c4260430ad90bf06cb5306f
samansoltani <34808539+samansoltani@users.noreply.github.com>
2019-02-25 19:12:35 -0500

    dice class update

--- SOEN6441/src/Model/Dice.java
+++ SOEN6441/src/Model/Dice.java
@@ -1,4 +1,7 @@
 package Model;
+
+import java.util.Random;
+
 /**
  * This class handles the dice actions.
  * @author samansoltani
@@ -6,5 +9,12 @@ package Model;
  */
 
 public class Dice {
-
+	/**
+	 * setting up a random value for dice. 
+	 * @return the value of dice as an integer.
+	 */
+	public int Roll() {
+		Random rand = new Random();
+		return rand.nextInt(6) + 1;
+	}
 }
